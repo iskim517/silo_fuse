a91ad8df38b4af462f2f1e060ee12cf122b25a1f
Inseob Kim <iskim517@gmail.com>
2016-06-04 14:17:16 +0900

    Add move ctr & assignment to block

--- block.h
+++ block.h
@@ -15,7 +15,9 @@ namespace silo
 	public:
 		explicit block(const char *dir);
 		block(const block &) = delete;
+		block(block &&) = default;
 		block &operator=(const block &) = delete;
+		block &operator=(block &&) = default;
 
 		chunk getchunk(const md5val &hash);
 		void addchunk(const chunk &chk, uint64_t initref);
