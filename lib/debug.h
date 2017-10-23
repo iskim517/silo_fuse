cae4163aefa5328be0fe5c5b79cebd59d95600de
iskim <iskim517@gmail.com>
2017-10-23 20:08:30 +0900

    Add default param

--- lib/debug.h
+++ lib/debug.h
@@ -2,4 +2,4 @@
 
 void print_backtrace();
 void safe_write(int fd, const void *buf, size_t len);
-bool safe_read(int fd, void *buf, size_t len, bool must_success);
+bool safe_read(int fd, void *buf, size_t len, bool must_success = false);
